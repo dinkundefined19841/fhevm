f79ed43ef0a2bbd9f675f72f7b6d3cd7a753ab8e
Antoniu Pop <antoniu.pop@zama.ai>
2025-07-18 14:36:24 +0100

    fix(coprocessor): add noise_squashing_compression_key for server key into/from raw parts

--- coprocessor/fhevm-engine/coprocessor/src/bin/utils.rs
+++ coprocessor/fhevm-engine/coprocessor/src/bin/utils.rs
@@ -33,7 +33,7 @@ pub fn extract_server_key_without_ns(src_path: String, dest_path: &String) -> bo
     let server_key: ServerKey = safe_deserialize_sns_key(&read(src_path).expect("read server key"))
         .expect("deserialize server key");
 
-    let (sks, kskm, compression_key, c, noise_squashing_key, tag) = server_key.into_raw_parts();
+    let (sks, kskm, compression_key, c, noise_squashing_key, noise_squashing_compression_key, tag) = server_key.into_raw_parts();
     if noise_squashing_key.is_none() {
         error!("Server key does not have noise squashing");
         return false;
@@ -47,6 +47,7 @@ pub fn extract_server_key_without_ns(src_path: String, dest_path: &String) -> bo
         compression_key,
         c,
         None, // noise squashing key excluded
+        None,
         tag,
     ));
 
